baf26c7e398c43d82b3052014d01392a0fb21cd0
Michel Lujano <lvm3632@gmail.com>
2021-02-17 03:05:09 +0000

    Cambios

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,11 @@
 #include <stdio.h> 
 
-
-int main(){
-	printf("Hello World\n");
+// y un asteristico solo es un arreglo
+// Doble asteristico es un arreglo de arreglo de caracteres
+int main(int numarg, char **argv){
+	
+	for(int i=1;i<numarg;i++)
+		printf("Hello World %s\n", argv[i]);
+	
+	
 }
