d65338925d2f456c89bfa08a4ec55b1c2ed21f77
Michel Lujano <a01636172@itesm.mx>
2021-05-26 00:56:06 +0000

    Practica 11

--- files/copiar.c
+++ files/copiar.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argnum, char **argv){
     char *original = argv[1];
     char *destino = argv[2];
+    int size=0;
     char buffer;
-    // Solamente leera bits
-    // size cantidad de caracteres a /m
     FILE *foriginal, *fdestino;
-    foriginal = fopen(original, "rb");
+    foriginal = fopen(original,"rb");
     fdestino = fopen(destino,"wb");
     while(fread(&buffer,sizeof(char),1,foriginal)){
         if(buffer == '\n'){
@@ -16,18 +16,30 @@ int main(int argnum, char **argv){
             size += 1;
             fseek(foriginal, -size, SEEK_CUR);
             linea = (char *) calloc(size, sizeof(char)); // linea[size];
-            fread(linea, sizeof(char), size, foriginal);
-            if(strstr(linea, "hola") == NULL){
-                fwrite(&buffer, sizeof(char),1,fdestino);
+            fread(linea,sizeof(char),size,foriginal);
+            if(strstr(linea,"hola") == NULL){
+                fwrite(linea,sizeof(char),size,fdestino);
             }
+            free(linea);
             size = 0;
-        }else{
+        } else 
             size += 1;
-        }
-        fwrite(&buffer, sizeof(char),1,fdestino);
-    } 
+        //fwrite(&buffer,sizeof(char),1,fdestino);
+    }
+    if(size!=0){
+        char *linea;
+        fseek(foriginal, -size, SEEK_CUR);
+        linea = (char *) calloc(size, sizeof(char)); // linea[size];
+        fread(linea,sizeof(char),size,foriginal);
+        if(strstr(linea,"hola") == NULL){
+            fwrite(linea,sizeof(char),size,fdestino);
+        } 
+        free(linea);
+        size = 0;
+    }
     fclose(foriginal);
     fclose(fdestino);
+
     return 0;
 }
 
